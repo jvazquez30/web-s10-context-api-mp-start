28564531d7d6451d2b2cf51b3a41682445ab088b
jvazquez30 <josevazquez881@gmail.com>
2024-05-27 16:06:15 -0700

    Added the quotesContext

--- frontend/context/quotesContext.js
+++ frontend/context/quotesContext.js
@@ -1,1 +1,15 @@
 // Create your context here
+import React from "react";
+import { createContext } from "react";
+
+const quotes = {
+    quotes: ['Hello there', 'Goodbye there']
+}
+
+export const QuotesContext = createContext()
+
+export const QuotesProvider = ({ children }) => (
+    <QuotesContext.Provider value={quotes}>
+        {children}
+    </QuotesContext.Provider>
+)
